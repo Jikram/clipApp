5bf742488358772b644816abc574ac679e660148
ji <jamshaid.ikram@gmail.com>
2022-09-22 21:40:46 -0500

    Added logout functionality & loginform enhancement

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
-import { map, Observable } from 'rxjs';
+import { delay, map, Observable } from 'rxjs';
 import Iuser from '../models/user.model';
 
 @Injectable({
@@ -10,13 +10,20 @@ import Iuser from '../models/user.model';
 export class AuthService {
   private userCollection: AngularFirestoreCollection<Iuser>;
   public isAuthenticated$ : Observable<boolean>;
+  public isAuthenticatedWithDelay$ : Observable<boolean>;
 
   constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
     this.userCollection = db.collection('users');
     //this.auth.user.subscribe(console.log)
+    
     this.isAuthenticated$ =  auth.user.pipe(
       map(user => !!user)
     )
+    
+    this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(
+      delay(1000)
+    )
+
   }
 
   async createUser(userData: Iuser) {
